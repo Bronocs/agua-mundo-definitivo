2fb15191108956abd7d8eb9ed09c71da2c591d86
Bronocs <79721734+Bronocs@users.noreply.github.com>
2025-06-06 16:10:53 -0500

    7.5

--- utils/algoliaClient.js
+++ utils/algoliaClient.js
@@ -5,5 +5,3 @@ export const searchClient = algoliasearch(
   TU_APPLICATION_ID,  // ← Tu Application ID de Algolia
   TU_SEARCH_API_KEY   // ← Tu Search-Only API Key de Algolia
 );
-
-export const productosIndex = searchClient.initIndex('productos'); // ← O el nombre que diste a tu índice
