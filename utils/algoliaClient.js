ec51af3617a2dc080ac3ae1a052a6d217f78efbc
Bronocs <79721734+Bronocs@users.noreply.github.com>
2025-06-06 16:12:26 -0500

    7.6

--- utils/algoliaClient.js
+++ utils/algoliaClient.js
@@ -2,6 +2,6 @@
 import algoliasearch from 'algoliasearch/lite';
 
 export const searchClient = algoliasearch(
-  TU_APPLICATION_ID,  // ← Tu Application ID de Algolia
-  TU_SEARCH_API_KEY   // ← Tu Search-Only API Key de Algolia
+  process.env.TU_APPLICATION_ID,  // ← Tu Application ID de Algolia
+  process.env.TU_SEARCH_API_KEY   // ← Tu Search-Only API Key de Algolia
 );
