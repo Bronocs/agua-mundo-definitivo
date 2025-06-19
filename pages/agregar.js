8308a36936e90d9c51a0d94de3540b43ccb49939
Bronocs <79721734+Bronocs@users.noreply.github.com>
2025-06-19 00:20:24 -0500

    aaa

--- pages/agregar.js
+++ pages/agregar.js
@@ -63,19 +63,12 @@ export default function Agregar() {
           <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4z"/>
           </svg>
         </button>
-        <button className={`${styles.iconBtn2} ${styles.btnCerrar2}`} onClick={enviarPedidos}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
-          <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z"/>
-          </svg>
-        </button>
-      </div>
-
-      <div style={{ margin: '1rem 0' }}>
-        <label style={{ fontWeight: 'bold' }}>Nombre del Proyecto:</label>
+        <div style={{ margin: '1rem 0' }}>
         <input
           type="text"
-          placeholder="Ejemplo: Planta Sur, Mantenimiento 2024, etc."
+          placeholder="Nombre del Proyecto"
           value={nombreProyecto}
+          className={styles.btnLibre2}
           onChange={e => setNombreProyecto(e.target.value)}
           style={{
             width: '100%',
@@ -86,6 +79,12 @@ export default function Agregar() {
             border: '1px solid #ccc'
           }}
         />
+        </div>
+        <button className={`${styles.iconBtn2} ${styles.btnCerrar2}`} onClick={enviarPedidos}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
+          <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z"/>
+          </svg>
+        </button>
       </div>
 
       <div style={{ marginTop: '2rem', width: '100%' }}>
