1f0ebf7b581a9d43d1c49fc77cc4de32ea6c0a89
Bronocs <79721734+Bronocs@users.noreply.github.com>
2025-06-19 00:26:14 -0500

    eeeeeee

--- pages/agregar.js
+++ pages/agregar.js
@@ -63,18 +63,16 @@ export default function Agregar() {
           <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4z"/>
           </svg>
         </button>
-        <div style={{ margin: '1rem 0' }}>
+        <div className={styles.btnLibre2} style={{ margin: '1rem 0' }}>
         <input
           type="text"
           placeholder="Nombre del Proyecto"
           value={nombreProyecto}
-          className={styles.btnLibre2}
           onChange={e => setNombreProyecto(e.target.value)}
           style={{
             width: '100%',
             padding: '0.8rem',
             fontSize: '1rem',
-            marginTop: '0.5rem',
             borderRadius: '6px',
             border: '1px solid #ccc'
           }}
