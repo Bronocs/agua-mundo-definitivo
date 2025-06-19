df2e9ad18597cd8d4e549f7bda685db0571e9939
Bronocs <79721734+Bronocs@users.noreply.github.com>
2025-06-19 17:02:36 -0500

    Updates "Ir al inicio" button to a close icon
    Replaces the text "Ir al inicio" on the return button with a close icon.
    
    Also, changes the background color to red and text color to orange for visual consistency.

--- pages/agregar.js
+++ pages/agregar.js
@@ -115,8 +115,8 @@ export default function Agregar() {
                 onClick={() => window.location.href = '/'}
                 style={{
                   marginTop: 18,
-                  background: "#0077cc",
-                  color: "#fff",
+                  background: "#ff6969",
+                  color: "#fc60000",
                   padding: "0.8rem 2.2rem",
                   border: "none",
                   borderRadius: "7px",
@@ -126,7 +126,9 @@ export default function Agregar() {
                   boxShadow: "0 2px 7px #c4d8ea80"
                 }}
               >
-                Ir al inicio
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                  <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+                </svg>                
               </button>
             </div>
           </div>
